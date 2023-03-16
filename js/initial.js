6eae4be2ef04ff922a3104bd44646b3227d8bcdd
Jacob <jacobxperez@gmx.com>
2023-03-16 12:36:18 -0600

    Update template.js

---
+++ js/initial.js
@@ -0,0 +1,23 @@
+/* @license
+ * Jacob Perez <https://jacobxperez.github.io/vannelli/>
+ * Copyright (C) 2022 Jacob Perez <jacobxperez@gmx.com>
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+-----------------------------------------------------------------------------*/
+const meta = {
+    layout: null,
+    title: null,
+    subtitle: null,
+    author: null,
+    date: {
+        publish: null,
+        revised: null,
+    },
+    getTitle() {
+        return (this.title = document.title);
+    },
+    getAuthor() {
+        return (this.author =
+            document.head.querySelector('[name="author"]').content);
+    },
+};
---
+++ js/modules/template.js
@@ -0,0 +1,94 @@
+class Template {
+    constructor(header, nav, main, article, section, aside, footer, layout) {
+        this.header = header;
+        this.nav = nav;
+        this.main = main;
+        this.article = article;
+        this.section = section;
+        this.aside = aside;
+        this.footer = footer;
+        this.layout = layout;
+        this.parser = new DOMParser();
+    }
+    #parseSource(source) {
+        // parse source
+        return this.parser.parseFromString(source, 'text/html');
+    }
+    #appendString(source, targetID) {
+        // get parsed source
+        const _parsedSource = this.#parseSource(source);
+        // get target id
+        const _targetID = document.getElementById(targetID);
+        // append parsed source to target id
+        _targetID.appendChild(_parsedSource.body);
+    }
+    #appendTemplate(source, templateID, targetID) {
+        // get source from template id
+        const _getTemplateID = source.getElementById(templateID);
+        // clone template id from source
+        const _cloneTemplate = _getTemplateID.content.cloneNode(true);
+        // get target id from document
+        const _targetID = document.getElementById(targetID);
+        // append template to target id
+        _targetID.appendChild(_cloneTemplate);
+        // delete original template from document
+        _getTemplateID.remove();
+    }
+    #parseTemplate(source, templateID, targetID) {
+        // get template source and parse it
+        const _parsedSource = this.#parseSource(source);
+        // append source template to target id
+        this.#appendTemplate(_parsedSource, templateID, targetID);
+    }
+    getTemplate(templateID, targetID, callback) {
+        new Promise((resolve, reject) => {
+            // check if template exist if not reject
+            templateID ? resolve() : reject();
+        })
+            .then(() => this.#appendTemplate(document, templateID, targetID))
+            .then(() => {
+                // optional: a callback function gets executed
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            })
+            .catch((err) => console.error(err, 'Error: Template not found'));
+
+        return this;
+    }
+    fromString(source, targetID, callback) {
+        new Promise((resolve, reject) => {
+            // check if source is string
+            typeof source === 'string'
+                ? resolve()
+                : reject((err = 'Error: Source is not a String'));
+        })
+            .then(() => this.#appendString(source, targetID))
+            .then(() => {
+                // optional: a callback function gets executed
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            })
+            .catch((err) => console.error(err));
+
+        return this;
+    }
+    fetchTemplate(source, targetID, callback) {
+        fetch(source)
+            // when the source is loaded convert to text
+            .then((response) => response.text())
+            .then((source) => this.#parseTemplate(source, targetID, targetID))
+            .then(() => {
+                // optional: a callback function gets executed
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            })
+            .catch(() => console.error('Error: Template not found'));
+
+        return this;
+    }
+}
+
+export {Template};
